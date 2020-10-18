c257fbf076a342a597bcdb3528e6574fda3d6922
gitgud5 <49618496+gitgud5@users.noreply.github.com>
2020-10-18 14:45:56 +0500

    Uploaded Pig Latin Converter File

---
+++ Pig Latin Converter.py
@@ -0,0 +1,41 @@
+"""
+Python code to Convert English Sentences to Pig Latin
+"""
+import sys
+
+
+def main():
+    sentence = input("What secret message you want to give?\n")
+    lis = sentence.split()
+    for i in range(len(lis)):
+        if (lis[i].lower().startswith('a')) or (lis[i].lower().startswith('e')) or (lis[i].lower().startswith('i')) or (
+                lis[i].lower().startswith('o')) or (lis[i].lower().startswith('u')):
+            lis[i] = changeWithVowel(lis[i])
+        else:
+            lis[i] = changeWithConsonant(lis[i])
+    print(" ".join(lis), file=sys.stderr)
+
+
+def changeWithVowel(sentence):
+    """
+    Takes word which starts with Vowel and converts it to pig latin
+    and returns it
+
+    :rtype: String object
+    """
+    return sentence + "way"
+
+
+def changeWithConsonant(sentence):
+    """
+    Take word which starts with a consonant and converts it to pig latin
+    and returns it
+
+    :rtype: object
+    """
+    a = sentence[0] + "ay"
+    return sentence[1:] + a
+
+
+if __name__ == '__main__':
+    main()
